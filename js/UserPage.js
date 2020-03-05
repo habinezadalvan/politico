b5a2832dc4a1d36d0b78b18339fd1abf55d1e6b8
giranezafiacre <fiacregiraneza@gmail.com>
2020-03-06 01:44:41 +0200

    other changes

--- js/UserPage.js
+++ js/UserPage.js
@@ -2,10 +2,15 @@ document.getElementById('vote').addEventListener('click', () =>
 {
     document.getElementById('vote-form').style.display = 'flex';
     document.getElementById('voting-form').style.display = 'flex';
+    document.getElementById('viewpart-form').style.display ='none';
+    document.getElementById('viewpol-form').style.display = 'none'
 });
 document.getElementById('viewparty').addEventListener('click', () =>
 {
-    document.getElementById('viewpart-form').style.display = 'flex';
+    document.getElementById('vote-form').style.display = 'none';
+    document.getElementById('voting-form').style.display = 'none';
+    document.getElementById('viewpart-form').style.display ='flex';
+    document.getElementById('viewpol-form').style.display = 'none'
 });
 document.getElementById("close-form").addEventListener('click', () =>
 {
@@ -13,5 +18,8 @@ document.getElementById("close-form").addEventListener('click', () =>
 });
 document.getElementById('viewpolitician').addEventListener('click', () =>
 {
+    document.getElementById('vote-form').style.display = 'none';
+    document.getElementById('voting-form').style.display = 'none';
+    document.getElementById('viewpart-form').style.display ='none';
     document.getElementById('viewpol-form').style.display = 'flex';
 });
