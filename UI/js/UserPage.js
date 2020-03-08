da0fd8947b65be9114387505a0fa42035c75fe1b
giranezafiacre <fiacregiraneza@gmail.com>
2020-03-08 15:57:57 +0200

    ch(folder_structure): change the folder structure

---
+++ UI/js/UserPage.js
@@ -0,0 +1,21 @@
+document.getElementById('vote').addEventListener('click', () =>
+{
+    document.getElementById('vote-form').style.display = 'flex';
+    document.getElementById('voting-form').style.display = 'flex';
+    document.getElementById('viewpart-form').style.display ='none';
+    document.getElementById('viewpol-form').style.display = 'none'
+});
+document.getElementById('viewparty').addEventListener('click', () =>
+{
+    document.getElementById('vote-form').style.display = 'none';
+    document.getElementById('voting-form').style.display = 'none';
+    document.getElementById('viewpart-form').style.display ='flex';
+    document.getElementById('viewpol-form').style.display = 'none';
+});
+document.getElementById('viewpol').addEventListener('click', () =>
+{
+    document.getElementById('vote-form').style.display = 'none';
+    document.getElementById('voting-form').style.display = 'none';
+    document.getElementById('viewpart-form').style.display ='none';
+    document.getElementById('viewpol-form').style.display = 'flex';
+});
