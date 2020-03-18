b022bdb8b135f1b865639aee1abe0891623d4e06
giranezafiacre <fiacregiraneza@gmail.com>
2020-03-18 04:54:51 +0200

    other changes of UI

--- UI/js/UserPage.js
+++ UI/js/UserPage.js
@@ -1,9 +1,13 @@
+
+
 document.getElementById('vote').addEventListener('click', () =>
 {
     document.getElementById('vote-form').style.display = 'flex';
     document.getElementById('voting-form').style.display = 'flex';
     document.getElementById('viewpart-form').style.display ='none';
-    document.getElementById('viewpol-form').style.display = 'none'
+    document.getElementById('viewpol-form').style.display = 'none';
+    document.getElementsByClassName('parag').style.display='none';
+    // document.getElementById('table1').style.display = 'none';
 });
 document.getElementById('viewparty').addEventListener('click', () =>
 {
@@ -11,6 +15,8 @@ document.getElementById('viewparty').addEventListener('click', () =>
     document.getElementById('voting-form').style.display = 'none';
     document.getElementById('viewpart-form').style.display ='flex';
     document.getElementById('viewpol-form').style.display = 'none';
+    document.getElementById('p').style.display='none';
+    // document.getElementById('table1').style.display = 'none';
 });
 document.getElementById('viewpol').addEventListener('click', () =>
 {
@@ -18,4 +24,28 @@ document.getElementById('viewpol').addEventListener('click', () =>
     document.getElementById('voting-form').style.display = 'none';
     document.getElementById('viewpart-form').style.display ='none';
     document.getElementById('viewpol-form').style.display = 'flex';
+    document.getElementsByClassName('parag').style.display='none';
+    // document.getElementById('table1').style.display = 'none';
 });
+document.getElementById('politico1').addEventListener('click', () =>{
+    document.getElementById('table1').style.display = 'flex';
+    document.getElementById('politico1').addEventListener('click', () =>{
+        document.getElementById('table1').style.display = 'none';
+        document.getElementById('politico1').addEventListener('click', () =>{
+            document.getElementById('table1').style.display = 'flex';
+            document.getElementById('politico1').addEventListener('click', () =>{
+                document.getElementById('table1').style.display = 'none';
+                document.getElementById('politico1').addEventListener('click', () =>{
+                    document.getElementById('table1').style.display = 'flex';
+                    document.getElementById('politico1').addEventListener('click', () =>{
+                        document.getElementById('table1').style.display = 'none';
+                        document.getElementById('politico1').addEventListener('click', () =>{
+                            document.getElementById('table1').style.display = 'flex';
+                            });
+                    });
+                });
+            });
+        });
+    });
+});
+
