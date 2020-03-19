3f9003287e178a406a55f5def1ef66d58cb79392
giranezafiacre <fiacregiraneza@gmail.com>
2020-03-19 11:59:36 +0200

    bug(fix) heroku logs[Fixes #171875618]

--- server/db/config.js
+++ server/db/config.js
@@ -2,5 +2,10 @@ import dotenv from 'dotenv';
 import { Pool } from 'pg';
 
 dotenv.config();
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+
+const { DATABASE_URL, NODE_ENV, TEST_DB_URL } = process.env;
+
+const db = (NODE_ENV === 'development' || 'production') ? DATABASE_URL : TEST_DB_URL;
+
+const pool = new Pool({ connectionString: db });
 export default pool;
